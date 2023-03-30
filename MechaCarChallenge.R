d3861ca7b86b47376c03511c2da1a3e66e4a0788
Kennan Foust <119345840+foukenn@users.noreply.github.com>
2023-03-30 15:05:31 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,31 @@
+#import dplyr
+library(dplyr)
+
+#Linear Regression to Predict MPG
+
+#reading csv in
+mechacarMPG_df <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+
+#Regression
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacarMPG_df)
+
+#P-Value and R-Squared
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacarMPG_df))
+
+
+#Suspension Coil Lot Analysis
+
+#reading csv in
+suspension_df <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+#summary frame
+total_summary <- suspension_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+#create summaries for each lot
+lots_summary <- suspension_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+# T-tests 
+
+t.test(subset(suspension_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
